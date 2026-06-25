ffb1961834bc42e1b568135bfce60c1e67aad5a4
jjcalle3 <jhoncas4610@hotmail.com>
2026-06-25 11:33:23 -0500

    obtener_operadora

--- ejemplo2/proyectoUno/administrativo/models.py
+++ ejemplo2/proyectoUno/administrativo/models.py
@@ -11,6 +11,9 @@ class Estudiante(models.Model):
         return "%s %s %s" % (self.nombre, 
                 self.apellido,
                 self.cedula)
+    
+    def obtener_nro_tel(self):
+        return len(NumeroTelefonico)
 
 class NumeroTelefonico(models.Model):
     telefono = models.CharField(max_length=100)
@@ -20,4 +23,21 @@ class NumeroTelefonico(models.Model):
 
     def __str__(self):
         return "%s %s" % (self.telefono, self.tipo)
+    
+    def obtener_operadora(self):
+        op = self.telefono[:3]
 
+        op_claro = [
+            "099", "098", "097"
+        ]
+
+        op_movistar = [
+            "096", "095"
+        ]
+
+        if op in op_claro:
+            return "Claro"
+        elif op in op_movistar:
+            return "Movistar"
+        else:
+            return "Operadora desconocida"
